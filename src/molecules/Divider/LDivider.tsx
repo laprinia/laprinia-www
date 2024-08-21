61a97f2d2a83692af65ed70a7ffbcb88a4e70d4e
laprinia <lavinialoghin7@gmail.com>
2024-08-21 19:41:39 +0300

    Reset structure

--- src/molecules/Divider/LDivider.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const DividerContainer = styled.section`
-  width: 100%;
-  margin-top: 4rem;
-`;
-const LDivider = () => <DividerContainer />;
-
-export default LDivider;
--- src/molecules/Divider/MDivider.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const DividerContainer = styled.section`
-  width: 100%;
-  margin-top: 2rem;
-`;
-const MDivider = () => <DividerContainer />;
-
-export default MDivider;
