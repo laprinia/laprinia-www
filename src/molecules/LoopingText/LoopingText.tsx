dbec396811b9fe75478ad1c791693942c22d2c8a
laprinia <lavinialoghin7@gmail.com>
2024-08-21 23:24:12 +0300

    Refactor fonts & LoopingText component

--- src/molecules/LoopingText/LoopingText.tsx
+++ src/molecules/LoopingText/LoopingText.tsx
@@ -1,43 +1,90 @@
-import styled from "styled-components";
 import React from "react";
+import styled, { keyframes } from "styled-components";
 
-const Looping = styled.div`
-  display: inline-block;
-  white-space: nowrap;
-  animation: scroll 30s linear infinite;
-  font-size: var(--font-size-M);
-  font-weight: var(--font-weight-bold);
-  color: var(--accent-color);
-  position: absolute;
-  will-change: transform;
-
-  span {
-    display: inline-flex;
-    align-items: end;
+const scroll = (isReverse: boolean) => keyframes`
+  0% {
+    transform: translateX(${isReverse ? "-50%" : "0%"});
   }
-
-  .asterisk {
-    margin: 0 0.5rem;
-    transform: translateY(35%);
-    font-size: var(--font-size-L);
-  }
-
-  @keyframes scroll {
-    0% {
-      transform: translateX(0%);
-    }
-    100% {
-      transform: translateX(-100%);
-    }
+  100% {
+    transform: translateX(${isReverse ? "0%" : "-50%"});
   }
 `;
 
+const ScrollingTextContainer = styled.div`
+  overflow: hidden;
+  position: relative;
+  width: 100%;
+  font-family: var(--font-heading);
+  font-weight: var(--font-weight-regular);
+  font-size: var(--font-size-M);
+  color: var(--accent-color);
+  margin-bottom: 1rem;
+`;
+
+const ScrollingTextInner = styled.div<{
+  marqueeSpeed: string;
+  isReverse: boolean;
+}>`
+  display: flex;
+  width: max-content;
+  animation: ${(props) => scroll(props.isReverse)}
+    ${(props) => props.marqueeSpeed} linear infinite;
+  white-space: nowrap;
+
+  &:hover {
+    animation-play-state: paused;
+  }
+`;
+
+const ScrollingText = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ScrollingTextItem = styled.div`
+  padding: 0 1.5rem;
+  display: flex;
+  align-items: center;
+`;
+
+const Divider = styled.span`
+  display: flex;
+  margin: 0 0.5rem;
+  transform: translateY(25%);
+  font-size: var(--font-size-L-heading);
+`;
+
 const LoopingText = ({
-  loopingTextContent,
+  items,
+  marqueeSpeed = "60s",
+  isReverse = false,
+  withDivider = false,
 }: {
-  loopingTextContent: React.ReactNode;
+  items: string[];
+  marqueeSpeed?: string;
+  isReverse?: boolean;
+  withDivider?: boolean;
 }) => {
-  return <Looping>{loopingTextContent}</Looping>;
+  return (
+    <ScrollingTextContainer>
+      <ScrollingTextInner marqueeSpeed={marqueeSpeed} isReverse={isReverse}>
+        <ScrollingText>
+          {items.map((item, index) => (
+            <React.Fragment key={index}>
+              <ScrollingTextItem>{item}</ScrollingTextItem>
+              {withDivider && index < items.length - 1 && <Divider>*</Divider>}
+            </React.Fragment>
+          ))}
+          {items.map((item, index) => (
+            <React.Fragment key={index + items.length}>
+              <ScrollingTextItem>{item}</ScrollingTextItem>
+              {withDivider && index < items.length - 1 && <Divider>*</Divider>}
+            </React.Fragment>
+          ))}
+        </ScrollingText>
+      </ScrollingTextInner>
+    </ScrollingTextContainer>
+  );
 };
 
 export default LoopingText;
