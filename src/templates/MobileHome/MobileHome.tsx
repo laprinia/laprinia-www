c744fe6b26d48da0471fb3a9d61aee6ce4656932
laprinia <lavinialoghin7@gmail.com>
2024-08-25 13:28:16 +0300

    Add workaround to make home not scrollable on mobile

--- src/templates/MobileHome/MobileHome.tsx
+++ src/templates/MobileHome/MobileHome.tsx
@@ -2,20 +2,21 @@ import HomeNavBar from "../../organisms/HomeNavBar/HomeNavBar";
 import { Canvas } from "@react-three/fiber";
 import CursorWaveTexture from "../../molecules/Texture/CursorWaveTexture";
 import { introductionItems } from "../../consts";
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 
 const MobileContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: calc(100vw - 5rem);
-  height: calc(100vh - 5rem);
+  height: calc(var(--vh, 1vh) * 100 - 5rem);
   margin: 2.5rem;
   gap: 1rem;
 
   @media (max-width: 767px) {
+    overflow: hidden;
     width: calc(100vw - 2rem);
-    height: calc(100vh - 2rem);
+    height: calc(var(--vh, 1vh) * 100 - 2rem);
     margin: 1rem;
     gap: 0.5rem;
   }
@@ -55,6 +56,20 @@ const CanvasSection = styled.div`
 `;
 
 const MobileHome = ({ mobileHeroPath }: { mobileHeroPath: string }) => {
+  useEffect(() => {
+    const updateVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+
+    updateVh();
+    window.addEventListener("resize", updateVh);
+
+    return () => {
+      window.removeEventListener("resize", updateVh);
+    };
+  }, []);
+
   return (
     <MobileContainer>
       <HomeNavBar isMobile />
