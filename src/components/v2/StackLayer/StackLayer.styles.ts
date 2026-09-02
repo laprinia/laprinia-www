7be49de5628a4ec1a5f83db581aff83722a8150b
laprinia <lavinia.dumitrenco@gmail.com>
2026-09-02 21:57:02 +0300

    feat: move to v4

--- src/components/v2/StackLayer/StackLayer.styles.ts
+++
@@ -1,9 +0,0 @@
-import { motion } from "framer-motion";
-import styled from "styled-components";
-
-export const Layer = styled(motion.div)`
-  position: sticky;
-  top: var(--layer-top, 0px);
-  overflow: hidden;
-  overflow-anchor: none;
-`;
